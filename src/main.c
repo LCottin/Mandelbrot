ef8b73327b8dc41bd9805dc43a7b4d3601b9fd2e
LCottin <cottin_leonard@icloud.com>
2022-03-06 17:13:16 +0100

    Adapted main to create a video

--- src/main.c
+++ src/main.c
@@ -5,21 +5,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "Image.h"
 #include "tools.h"
 
 int main(int argc, char *argv[])
 {
-    // Create a Mandlebrot image
-    Image mandlebrot_image = create_mandlebrot_image(900, 600, -2, 1, 1, -1);
+    // Creates a serie of files
+    float start_x1 = -2;
+    float start_y1 = 1;
+    float start_x2 = 1;
+    float start_y2 = -1;
+    const int iterations = 50;
 
-    int success = write_image(&mandlebrot_image, "imgs/PPM/mandlebrot_image.ppm");
+    const float goal_x = -0.99;
+    const float goal_y = 0.3;
+    const float step_x1 = (goal_x - start_x1) / iterations;
+    const float step_y1 = (goal_y - start_y1) / iterations;
+    const float step_x2 = (goal_x - start_x2) / iterations;
+    const float step_y2 = (goal_y - start_y2) / iterations;
 
-    if (success == 0)
-        printf("Image written successfully!\n");
-    else
-        printf("Error writing image!\n");
+    char filename[40];
+    char new_filename[40];
+    
+    for (int i = 0; i < iterations; i++)
+    {
+        // Create a Mandlebrot image
+        Image mandlebrot_image = create_mandlebrot_image(900, 600, start_x1, start_y1, start_x2, start_y2);
+
+        // Write the image to a file
+        memset(filename, 0, sizeof(filename));
+        memset(new_filename, 0, sizeof(new_filename));
+        sprintf(filename, "imgs/PPM/mandlebrot_video%d%d.ppm", i/10, i%10);
+        sprintf(new_filename, "imgs/JPG/mandlebrot_video%d%d.jpg", i/10, i%10);
+        write_image(&mandlebrot_image, filename, new_filename);
+
+        // Update the start coordinates
+        start_x1 += step_x1;
+        start_y1 += step_y1;
+        start_x2 += step_x2;
+        start_y2 += step_y2;
+    }
    
+    system("ffmpeg -framerate 10 -i imgs/PPM/mandlebrot_video%2d.ppm imgs/video/video.avi");
     return 0;
 }
